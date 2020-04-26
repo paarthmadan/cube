e470ea8781cff87874d945ce82de4e39a839f2c5
Paarth Madan <paarth.madan@shopify.com>
2020-04-26 17:45:22 -0400

    Wireframe UI complete, with sample data

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::io;
 use termion::{input::MouseTerminal, raw::IntoRawMode, screen::AlternateScreen};
 use tui::Terminal;
 use tui::backend::TermionBackend;
-use tui::widgets::{Paragraph, Text, Widget, Block, Borders, SelectableList};
+use tui::widgets::{Paragraph, Text, Widget, Block, Borders, SelectableList, Axis, Chart, Dataset, Marker};
 use tui::layout::{Layout, Constraint, Direction, Alignment};
 use tui::style::{Color, Style, Modifier};
 
@@ -11,7 +11,8 @@ struct SampleData {
     time_string: String,
     cube_type_string: String,
     last_10_solves: Vec<f32>,
-    points: Vec<(u32, u32)>,
+    average_text: Vec<String>,
+    points: Vec<(f64, f64)>,
 }
 
 impl SampleData {
@@ -21,12 +22,19 @@ impl SampleData {
             time_string: "12.34".to_string(),
             cube_type_string: "3x3".to_string(),
             last_10_solves: vec![12.34, 6.54, 5.55, 6.24, 21.54, 10.00, 64.32, 10f32, 4.44, 3.33],
+            average_text: vec![
+                "ao5: 25.11".to_string(),
+                "ao12: 25.11".to_string(),
+                "ao50: 25.11".to_string(),
+                "ao100: 25.11".to_string(),
+                "stdev: 2.55".to_string(),
+            ],
             points: vec![
-                (1, 1),
-                (2, 2),
-                (3, 1),
-                (4, 3),
-                (5, 7),
+                (1.0, 1.0),
+                (2.0, 2.0),
+                (3.0, 1.0),
+                (4.0, 3.0),
+                (5.0, 7.0),
             ]
         }
     }
@@ -118,5 +126,36 @@ fn main() -> Result<(), io::Error> {
             .block(Block::default().title("Recent Solves").borders(Borders::ALL))
             .alignment(Alignment::Left)
             .render(&mut f, recent);
+
+        let text: Vec<Text> = data.average_text.iter().map(|s| Text::styled(s.to_string() + "\n", Style::default().fg(Color::White))).collect();
+
+        Paragraph::new(text.iter())
+            .block(Block::default().title("Average").borders(Borders::ALL))
+            .alignment(Alignment::Left)
+            .render(&mut f, averages);
+
+        let dataset = Dataset::default()
+            .name("All time solves")
+            .marker(Marker::Dot)
+            .style(Style::default().fg(Color::Cyan))
+            .data(&data.points);
+
+
+        Chart::default()
+            .block(Block::default().title("Solves").borders(Borders::ALL))
+            .x_axis(Axis::default()
+                .title("Solve")
+                .title_style(Style::default().fg(Color::Red))
+                .style(Style::default().fg(Color::White))
+                .bounds([0.0, 10.0])
+                .labels(&["0.0", "5.0", "10.0"]))
+        .y_axis(Axis::default()
+            .title("Time")
+            .title_style(Style::default().fg(Color::Red))
+            .style(Style::default().fg(Color::White))
+            .bounds([0.0, 10.0])
+            .labels(&["0.0", "5.0", "10.0"]))
+        .datasets(&[dataset])
+        .render(&mut f, graph);
     })
 }
